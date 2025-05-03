bb77d642ff70a822e14e8d8fc2f79b12576b31af
faisgit <fa830163@gmail.com>
2025-05-03 19:35:16 +0530

    First Day

---
+++ index.py
@@ -0,0 +1,38 @@
+# a = 3
+# b = a
+# print(a)
+# print(b)
+# a = 4
+# print(b)
+
+
+
+# name = input("Enter your name:- ")
+# print(f"my name is {name}")
+
+
+# a = int(input("Enter First No:  "))
+# b = int(input("Enter Second No: "))
+# print(f"{a} + {b} = {a + b}")
+
+# Write a Code which gives grade to students according to their grades
+# 90-100 --> Grade A
+# 70-89 --> Grade B
+# 60-69 --> Grade C
+# 50-59 --> Grade D 
+# 0-49 --> Grade E
+
+
+# print("ell world")
+
+# from ab import a
+# print(a)
+
+# a = "abcd"
+# print(bool(a))
+
+# print(9 == int(9.3))
+
+print(0 == True)
+
+print(len("abc") == len("pqr"))
