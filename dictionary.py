254a39527fc047e9f836e1c6edef9de88f8a3374
faisgit <fa830163@gmail.com>
2025-05-13 18:12:34 +0530

    make changes

---
+++ dictionary.py
@@ -0,0 +1,33 @@
+person = {
+    "name": "John Doe",
+    "age": 30,
+    "city": "New York",
+    "is_student": False,
+    "courses": ["Math", "Science", "History"],
+    "address": {
+        "street": "123 Main St",
+        "city": "New York",
+        "state": "NY",
+        "zip": "10001"
+    }
+    
+}
+
+# print(person["address"]["street"])
+
+# print(len(person['address']))
+
+# add
+
+person['phNo'] = "1234567890"
+# print(person)
+# print(type(person['phNo']))
+
+# Edit
+
+# person['is_student'] = True
+# print(int(person['is_student']))
+
+print(0)
+print(int(False))
+
---
+++ tutples_and_sets.py
@@ -0,0 +1,34 @@
+# empty_tuple = ()
+# print(type(empty_tuple))
+
+# tpl = "abccde"
+# print(type(tpl))
+
+
+# tpl = ('item1', 'item2','item3')
+
+# print(len(tpl))
+# print(tpl[0])
+# print(tpl[-1])
+
+
+# fruits = ('banana', 'orange', 'mango', 'lemon')
+# fruits = list(fruits)
+# fruits[0] = 'apple'
+# print(fruits)   
+# fruits = tuple(fruits)
+# print(fruits) 
+
+# print("mang" in fruits)
+
+
+# -------------------------------- SETS --------------------------------
+
+
+# st = {'item1'}
+# print(type(st))
+
+
+
+t = "hsagdhgasfhgdas"  # Fixed: Assigning a string value to t
+
