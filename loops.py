d9ab6b9d1a4b50d16c145b199851845d6247c015
faisgit <fa830163@gmail.com>
2025-05-23 10:44:50 +0530

    loops

--- loops.py
+++ loops.py
@@ -24,15 +24,46 @@
 # else:
 #     print(a)
 
-marks = [78, 65, 90, 65, 88, 92, 65, 76, 81, 65, 59, 65, 73, 85]
+# marks = [78, 65, 90, 65, 88, 92, 65, 76, 81, 65, 59, 65, 73, 85]
 # marksListLen = len(marks)
-z = 0
-while(True):
-    if(z == 65):
-        z = z+ 1
-        continue
-    else:
-        print(z)
-        z = z+ 1
-        
-    
+# z = 0
+# while(True):
+#     if(z == 65):
+#         z = z+ 1
+#         continue
+#     else:
+#         print(z)
+#         z = z+ 1
+
+
+# for(z  in )
+# x = ""       
+# for i in "chetanya":
+#     x+=i
+# print(x)
+# min = int(input("Enter a minimum number : "))
+# max = int(input("Enter a maximum number : "))
+# num = int(input("Enter a number: "))
+# for i in range(min,max + 1):
+#     print(i * num)
+
+person = {
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_marred': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+}
+
+# for key in person:
+#     print(key)
+
+for key, value in person.items():
+    if(key == 'first_name' or key == 'last_name' or key == 'skills'):
+        print(f"{key} - {value}")
+
